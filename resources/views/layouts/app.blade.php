40dca3b5b0d56ed324835a4a07327549cc579234
tsuengineer <ootubom@gmail.com>
2024-09-21 16:03:59 +0900

    タイトルタグを修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>@yield('title')</title>
 
         <!-- OGP -->
         <meta property="og:title" content="@yield('title')" />
